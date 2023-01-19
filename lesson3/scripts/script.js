d7f821fe656464bef0e302f3bd4819f2b23134ac
bloominolive <bloomin.olive88@gmail.com>
2023-01-19 18:10:49 -0500

    lesson3 complete for week

---
+++ lesson3/scripts/script.js
@@ -0,0 +1,5 @@
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector("#curYr").innerHTML = year;
+let lastModified = new Date(document.lastModified);        
+document.getElementById("docLastUpdated").innerHTML = lastModified
