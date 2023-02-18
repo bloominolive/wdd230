b5b1e7cfea4165f57b1b5382b913cec9ae52d492
bloominolive <bloomin.olive88@gmail.com>
2023-02-18 06:01:12 -0500

    almost done creating discover page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,4 @@
+
 const datefieldUK = document.getElementById("date");
 const now = new Date();
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
@@ -26,3 +27,50 @@ var dayOfWeek = today.getDay();
 if (dayOfWeek == 1 || dayOfWeek == 2) {
   document.getElementById("meeting-banner").style.display = "block";
 }
+
+
+
+const currentDate = new Date();
+const lastVisit = localStorage.getItem('lastVisit');
+if (!lastVisit) {
+  localStorage.setItem('lastVisit', currentDate.toString());
+} else {
+  const lastVisitDate = new Date(lastVisit);
+  const timeDiff = currentDate.getTime() - lastVisitDate.getTime();
+  const daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+  document.getElementById("visits").innerHTML = `<em> ${daysDiff} </em>`;
+  localStorage.setItem('lastVisit', currentDate.toString());
+}
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(image) {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+      image.removeAttribute("data-src");
+    };
+}
+
+const imgOptions = {
+    threshold : 1,
+    rootMargin : "0px 0px 2px 0px"
+};
+  
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                preloadImage(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+    images.forEach(image => {
+        imgObserver.observe(image);
+    });
+}else {
+    images.forEach(image => {
+        preloadImage(image);
+    });
+}
+
