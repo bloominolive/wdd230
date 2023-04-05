d39644a7bf1bae4ecc82a51982a47523cf1065b6
bloominolive <bloomin.olive88@gmail.com>
2023-04-04 20:00:21 -0400

    update

--- bountiful/scripts/cookie.js
+++ bountiful/scripts/cookie.js
@@ -7,11 +7,8 @@ const cookieValue = document.cookie
 const orderQuantity = parseInt(cookieValue);
 
 const orderQuantitySpan = document.getElementById('order-quantity');
-orderQuantitySpan.textContent = orderQuantity;
 
 // Check if the integer value was successfully retrieved from the cookie
 if (!isNaN(orderQuantity)) {
-    console.log(`The value of myInt is ${orderQuantity}`);
-} else {
-    console.log(`Where is the cookie?`);
+    orderQuantitySpan.textContent = orderQuantity;
 }
